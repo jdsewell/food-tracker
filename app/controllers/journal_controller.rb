cef222e6fffe67a4d4d3bf08649f7a8165f561a3
jdsewell <jdsewell@protonmail.com>
2019-02-22 15:51:01 -0500

    Developing home journal page

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -1,4 +1,11 @@
 class JournalController < ApplicationController
   def home
+    @journal = current_user.journal
+    Day.all.each do |day|
+      if day.user_id == current_user.id
+        current_user.journal.push(day)
+        current_user.save
+      end
+    end
   end
 end
