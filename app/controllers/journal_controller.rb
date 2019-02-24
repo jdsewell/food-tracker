d1fe65e8727e691d47a32bc504d7d692d99457aa
jdsewell <jdsewell@protonmail.com>
2019-02-23 23:25:26 -0500

    Edited journal

--- app/controllers/journal_controller.rb
+++ app/controllers/journal_controller.rb
@@ -1,11 +1,11 @@
 class JournalController < ApplicationController
   def home
-    @journal = current_user.journal
     Day.all.each do |day|
-      if day.user_id == current_user.id
+      if day.user_id == current_user.id && current_user.journal.include?(day) == false
         current_user.journal.push(day)
         current_user.save
       end
     end
+    @journal = current_user.journal
   end
 end
